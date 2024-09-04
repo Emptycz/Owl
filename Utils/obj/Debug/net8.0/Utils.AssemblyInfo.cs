e28db9740fca3f6bc4228afc4aad205938a96de7
Jan Kočvara <kimichisxd285@gmail.com>
2024-09-04 19:37:56 +0200

    MISC

--- Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
+++ Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8304b1bc8e26f52a1f396b1992f5ed466c84c1f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6fe7bf0d753d0bdf0951f49cf71a279305863b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
