f2ffa911b9d473d68512bdd678a0743f291700e6
Jan Kočvara <kimichisxd285@gmail.com>
2024-09-15 18:51:09 +0200

    wip changes in `VariableManager` & `RequestViewModel`

--- Owl/Owl/ViewModels/RequestViewModel.cs
+++ Owl/Owl/ViewModels/RequestViewModel.cs
@@ -99,15 +99,14 @@ public partial class RequestViewModel : ViewModelBase
 	partial void OnSearchChanging(string value)
 	{
 		Requests = new ObservableCollection<RequestNodeVm>(_nodeRepository
-			.Find((x) => x.Name.Contains(value))
+			.Find(x => x.Name.Contains(value))
 			.Select(r => new RequestNodeVm(r)));
 	}
 
 	[RelayCommand]
 	private void AddHeader()
 	{
-		if (RequestState.Current is null) return;
-		RequestState.Current.Headers.Add(new RequestHeader());
+		RequestState.Current?.Headers.Add(new RequestHeader());
 		// SelectedRequest.Headers.Add(newHeader.Key, newHeader.Value);
 		// HeadersCollection.Add(newHeader);
 	}
@@ -156,6 +155,7 @@ public partial class RequestViewModel : ViewModelBase
 		var requestVariables = VariableFinder.ExtractVariables(request);
 		foreach (FoundVariable foundVariable in requestVariables)
 		{
+			var resolvedVariable = VariableManager.GetVariableValue(foundVariable.Key, _environmentState.Current?.Name);
 			var variable = _environmentState.GetVariables().SingleOrDefault(x => x.Key == foundVariable.Key);
 			if (variable is null)
 			{
@@ -225,6 +225,7 @@ public partial class RequestViewModel : ViewModelBase
 		{
 			ResponseTime = string.Empty;
 
+			Console.WriteLine(ex);
 			// Handle any exceptions, such as network errors or JSON parsing errors
 			Console.WriteLine($"Error sending request: {ex.Message}");
 			ResponseStatus = HttpStatusCode.InternalServerError;
@@ -236,28 +237,32 @@ public partial class RequestViewModel : ViewModelBase
 	{
 		ResponseSize = SizeCalc.CalculateSize(response.Content.ReadAsStringAsync().Result, Encoding.ASCII).ToString();
 		if (RequestState.Current is null) return;
-
-		// TODO: This should be Variable business logic implementation
-		string json = response.Content.ReadAsStringAsync().Result;
-		var parsedJson = JsonSerializer.Deserialize<JsonElement>(json);
-
-		// Example path provided by the user
-		const string userPath = "authentication.strategy"; // Example string path
-
-		// Extract value based on string path
-		object? valueFromStringPath = JsonTraverser.TraverseJson(parsedJson, userPath);
-		Console.WriteLine(valueFromStringPath); // Output: value at the specified path
-
 		RequestState.Current.Response = response;
+
+		// // TODO: This should be Variable business logic implementation
+		// string json = response.Content.ReadAsStringAsync().Result;
+		// var parsedJson = JsonSerializer.Deserialize<JsonElement>(json);
+		//
+		// // Example path provided by the user
+		// const string userPath = "authentication.strategy"; // Example string path
+		//
+		// // Extract value based on string path
+		// object? valueFromStringPath = JsonTraverser.TraverseJson(parsedJson, userPath);
+		// Console.WriteLine(valueFromStringPath); // Output: value at the specified path
 	}
 
-	private UserControl GetResponseControl(HttpResponseMessage? response) =>
-		response?.Content.Headers.ContentType?.MediaType switch
+	private UserControl GetResponseControl(HttpResponseMessage? response)
+	{
+		Console.WriteLine(response?.Content.Headers.ContentType?.MediaType);
+		return response?.Content.Headers.ContentType?.MediaType switch
 		{
 			"application/xml" => new RawResponseTab(RequestState),
 			"application/json" => new JsonResponseTab(RequestState),
+			// TODO: We can use system default webView to try to render the HTML page
+			// "text/html" => new HtmlResponseTab(),
 			_ => new RawResponseTab(RequestState),
 		};
+	}
 
 	private UserControl GetTabControl(int tabIndex)
 	{
